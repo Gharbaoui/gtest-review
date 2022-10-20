90106c4f20eb36523ac4615c00c94e328ecc5960
Gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-20 09:15:39 +0000

    this is how to create mock class

---
+++ mock/MockBankServer.hpp
@@ -0,0 +1,20 @@
+#ifndef __MOCK_BANK_SERVER__
+#define __MOCK_BANK_SERVER__
+#include "./BankServer.hpp"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+using ::testing::AtLeast;
+using ::testing::Return;
+
+
+class   MockBankServer : public BankServer {
+    public:
+        MOCK_METHOD(void, connect, (), (override));
+        MOCK_METHOD(void, disconnect, (), (override));
+        MOCK_METHOD(void, credit, (unsigned, int), (override));
+        MOCK_METHOD(void, debit, (unsigned, int), (override));
+        MOCK_METHOD(bool, doubleTransaction, (int, int, int), (override));
+        MOCK_METHOD(int, getbalance, (int), (const, override));
+};
+
+#endif
