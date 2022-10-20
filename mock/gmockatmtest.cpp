e06be14dce7068ae2269d97696b8bef9dd003683
Gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-20 09:16:05 +0000

    using mock class

---
+++ mock/gmockatmtest.cpp
@@ -0,0 +1,18 @@
+#include "./AtmMachine.hpp"
+#include "./MockBankServer.hpp"
+
+TEST(AtmMachine, basicTests)
+{
+    unsigned account_number = 6790;
+    int value = 1000;
+    MockBankServer bankServer;
+
+    EXPECT_CALL(bankServer, connect()).Times(1);
+    EXPECT_CALL(bankServer, getbalance(account_number)).Times(1).WillOnce(Return(999));
+    EXPECT_CALL(bankServer, debit(account_number, value)).Times(1);
+    EXPECT_CALL(bankServer, disconnect()).Times(1);
+
+    AtmMachine atm(&bankServer);
+    bool res = atm.withdraw(account_number, value);
+    EXPECT_TRUE(res);
+}
